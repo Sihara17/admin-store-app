64f7efe75b9c481ec6a224ad3e2335763546e70d
Sihara17 <147967440+Sihara17@users.noreply.github.com>
2025-07-31 21:32:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const produkRoutes = require("./routes/produk");
+const pembelianRoutes = require("./routes/pembelian");
+
 const app = express();
-const path = require('path');
-const bodyParser = require('body-parser');
-const produkRoutes = require('./routes/produk');
-const pembelianRoutes = require('./routes/pembelian');
-const db = require('./db'); // koneksi PostgreSQL pakai pg
 
-// Tes koneksi database
-db.connect((err) => {
-  if (err) {
-    console.error('❌ Database connection error:', err.stack);
-  } else {
-    console.log('✅ Connected to PostgreSQL database');
-  }
-});
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', produkRoutes);
-app.use('/pembelian', pembelianRoutes);
+app.use("/", produkRoutes);
+app.use("/pembelian", pembelianRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
