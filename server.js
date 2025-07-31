f2ae0cfdc6aa57cb4e0a92997c61a630b6b33892
Sihara17 <147967440+Sihara17@users.noreply.github.com>
2025-08-01 00:57:49 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+// server.js
+const express = require("express");
+const path = require("path");
+const produkRouter = require("./routes/produk");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// routing
+app.get("/", (req, res) => res.send("Homepage aktif ✅"));
+app.use("/produk", produkRouter);
+
+// listen
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
