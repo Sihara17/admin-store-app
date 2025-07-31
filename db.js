67cdd9681426820294cfc6c10172011d33156553
Sihara17 <147967440+Sihara17@users.noreply.github.com>
2025-07-31 18:37:09 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,18 @@
 const mysql = require('mysql2');
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'admin_store'
+  database: 'admin_store_db'
 });
 
 connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to MySQL');
+  if (err) {
+    console.error('Database connection error:', err);
+  } else {
+    console.log('Connected to MySQL database.');
+  }
 });
 
 module.exports = connection;
