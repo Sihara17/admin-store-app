ac2f5d92ad9c4b67c49a294222fb89c5cc6eecd4
Sihara17 <147967440+Sihara17@users.noreply.github.com>
2025-08-01 00:04:04 +0700

    Update produk.js

--- routes/produk.js
+++ routes/produk.js
@@ -4,19 +4,24 @@ const db = require('../db');
 
 // GET semua produk
 router.get('/', (req, res) => {
-  db.query('SELECT * FROM produk', (err, results) => {
+  db.query('SELECT * FROM products', (err, results) => {
     if (err) return res.status(500).send(err);
-    res.render('produk', { produk: results });
+    res.render('produk', { produk: results.rows });
   });
 });
 
 // POST tambah produk baru
 router.post('/tambah', (req, res) => {
-  const { nama, harga } = req.body;
-  db.query('INSERT INTO produk (nama, harga) VALUES (?, ?)', [nama, harga], (err) => {
-    if (err) return res.status(500).send(err);
-    res.redirect('/produk');
-  });
+  const { nama, harga, stok } = req.body;
+
+  db.query(
+    'INSERT INTO products (name, price, stock) VALUES ($1, $2, $3)',
+    [nama, harga, stok],
+    (err) => {
+      if (err) return res.status(500).send(err);
+      res.redirect('/produk');
+    }
+  );
 });
 
 module.exports = router;
