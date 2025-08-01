2eab9340d7bf6296f54ea68be519e75b6026fce8
Sihara17 <147967440+Sihara17@users.noreply.github.com>
2025-08-01 13:25:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
-// server.js
 const express = require("express");
+const app = express();
 const path = require("path");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 const produkRouter = require("./routes/produk");
 const pembelianRouter = require("./routes/pembelian");
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// view engine
-app.set("view engine", "ejs");
+// View engine
 app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-// routing
-app.get("/", (req, res) => res.send("Homepage aktif ✅"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.redirect("/produk");
+});
 app.use("/produk", produkRouter);
 app.use("/pembelian", pembelianRouter);
 
-// listen
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+// Jangan listen di Vercel
+if (process.env.NODE_ENV !== "production") {
+  app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+  });
+}
+
+module.exports = app;
